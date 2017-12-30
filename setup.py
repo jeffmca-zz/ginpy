8d6060bd855354bb467be6e6164252c8b623372c
Jeff McAdams <jmcadams@appriss.com>
2017-12-30 16:27:34 -0500

    Add a setup.py file to make it a better behaved Python package.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "ginpy",
+        version = "0.0",
+        description = "Pythonic Object Oriented Interface to Juniper Devices",
+        author = "Jeff McAdams",
+        )
